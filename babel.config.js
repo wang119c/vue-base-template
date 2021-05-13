e8b46163f71defac8e6b6553d515be893e9e1dc6
huizi <1052430943@qq.com>
2021-05-13 17:25:57 +0800

    新增element,loadsh,axios

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   presets: [
+    // https://github.com/vuejs/vue-cli/tree/master/packages/@vue/babel-preset-app
     '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    [
+      'component',
+      {
+        libraryName: 'element-ui',
+        styleLibraryName: 'theme-chalk'
+      }
+    ]
   ]
 }
